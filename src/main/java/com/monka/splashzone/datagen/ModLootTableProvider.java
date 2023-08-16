e74a87923a298e6948b1cc2e35b702f884d0c4c2
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-08-16 05:32:31 -0400

    Added Entity LootTable Datagen

--- src/main/java/com/monka/splashzone/datagen/ModLootTableProvider.java
+++ src/main/java/com/monka/splashzone/datagen/ModLootTableProvider.java
@@ -1,17 +1,20 @@
 package com.monka.splashzone.datagen;
 
 import com.monka.splashzone.datagen.server.ModBlockLootTables;
+import com.monka.splashzone.datagen.server.ModEntityLootTables;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.Tags;
 
 import java.util.List;
 import java.util.Set;
 
 public class ModLootTableProvider {
     public static LootTableProvider create(PackOutput output) {
-        return new LootTableProvider(output, Set.of(),
-                List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)));
-
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK),
+                new LootTableProvider.SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)));
     }
+
 }
