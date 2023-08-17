b1eb945de623b3efdea9c868abadbecc9625ffc6
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-08-17 04:48:10 -0400

    Ugg Egg no longer drops XP

--- src/main/java/com/monka/splashzone/entity/UggEggEntity.java
+++ src/main/java/com/monka/splashzone/entity/UggEggEntity.java
@@ -16,15 +16,17 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class UggEggEntity extends AgeableMob {
-    public UggEggEntity(EntityType<? extends AgeableMob> pEntityType, Level pLevel) {
+public class UggEggEntity extends Animal {
+    public UggEggEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
+        this.skipDropExperience();
     }
 
     public UggEggEntity(Level level, double x, double y, double z) {
