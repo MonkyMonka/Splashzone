9a954a627a6ce868b8f3e2663e7066a7956f47da
MonkyMonka <daniyaldanish11dd@gmail.com>
2023-08-17 04:56:14 -0400

    Removed Ugg Egg Entity LootTable

--- src/main/java/com/monka/splashzone/datagen/server/ModEntityLootTables.java
+++ src/main/java/com/monka/splashzone/datagen/server/ModEntityLootTables.java
@@ -1,15 +1,10 @@
 package com.monka.splashzone.datagen.server;
 
 import com.monka.splashzone.registry.EntityRegistry;
-import com.monka.splashzone.registry.ItemRegistry;
 import net.minecraft.data.loot.EntityLootSubProvider;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.predicates.LootItemKilledByPlayerCondition;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.stream.Stream;
@@ -22,14 +17,15 @@ public class ModEntityLootTables extends EntityLootSubProvider {
 
     @Override
     public void generate() {
-        this.add(EntityRegistry.UGG_EGG_ENTITY.get(), LootTable.lootTable()
-                .withPool(LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
-                        .add(LootItem.lootTableItem(ItemRegistry.UGG_EGG.get())
-                                .when(LootItemKilledByPlayerCondition.killedByPlayer())
-                        )
-                )
-        );
+//        this.add(EntityRegistry.UGG_EGG_ENTITY.get(), LootTable.lootTable()
+//                .withPool(LootPool.lootPool()
+//                        .setRolls(ConstantValue.exactly(1))
+//                        .add(LootItem.lootTableItem(ItemRegistry.UGG_EGG.get())
+//                                .when(LootItemKilledByPlayerCondition.killedByPlayer())
+//                        )
+//                )
+//        );
+        this.add(EntityRegistry.UGG_EGG_ENTITY.get(), LootTable.lootTable());
         this.add(EntityRegistry.UGG_ENTITY.get(), LootTable.lootTable());
     }
 
